7ec154c65c9e07ed9f7cf469af02239f43822afc
Vic Wang <vic.leiwang@gmail.com>
2023-02-07 23:14:46 +0000

    Added corsConfigurer

--- src/main/java/dev/vicwang/movies/MoviesApplication.java
+++ src/main/java/dev/vicwang/movies/MoviesApplication.java
@@ -1,17 +1,28 @@
 package dev.vicwang.movies;
 
-import dev.vicwang.movies.repository.MovieRepository;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoviesApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MoviesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false).maxAge(3600);
+            }
+        };
+    }
 }
